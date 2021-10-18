e102e2e06ef33550ac19abacb05a26d0d6830eab
Prashant <prashantacharya@lftechnology.com>
2021-10-18 14:41:15 +0545

    If backup file name is not supplied, set a default

--- backup.sh
+++ backup.sh
@@ -2,21 +2,24 @@
 
 echo "Creating backup of $1"
 
+# Copy the source directory in a backup directory
 backup_dir="$1-$(date +'%m-%d-%Y')"
-
 cp -r $1 $backup_dir
 
+# Delete miscellaneous directories like .git and .vs
 delete_misc_dirs() {
   rm -rf $backup_dir/.git*
   rm -rf $backup_dir/.vs
 }
 
+# Delete all node_modules
 recursively_delete_node_modules() {
   echo ""
   echo "****** Deleting node_modules ******"
   find $backup_dir -name 'node_modules' -type d -prune -exec echo '{}' \; -exec rm -rf {} \; 
 }
 
+# Delete .env files if present 
 recursively_delete_env() {
   echo ""
   echo "****** Deleting .env files ******"
@@ -29,10 +32,18 @@ delete_misc_dirs
 recursively_delete_node_modules
 recursively_delete_env
 
-#zip_file_name="$(basename -- $backup_dir).zip"
-zip_file_name="$2.zip"
+# if backup file name is not supplied
+# set default value (dirname-date.zip)
+if [ -z "$2" ]
+  then
+    zip_file_name="$(basename -- $backup_dir).zip"
+  else 
+    zip_file_name="$2.zip"
+fi
 
+# Create zip file and delete backup directory
 zip -9 -y -r -q $zip_file_name $backup_dir
-
-echo "Created ZIP: $zip_file_name"
 rm -rf $backup_dir
+
+echo ""
+echo "Created ZIP: $zip_file_name"
